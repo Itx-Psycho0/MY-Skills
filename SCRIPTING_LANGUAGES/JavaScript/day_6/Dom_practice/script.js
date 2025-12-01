10eb89753f5bf1d8f28cc4e5cc762f7fdc956189
Anurag Singh <anuragh3re@gmail.com>
2025-12-01 13:14:50 +0530

    fix bug

--- SCRIPTING_LANGUAGES/JavaScript/day_6/Dom_practice/script.js
+++ SCRIPTING_LANGUAGES/JavaScript/day_6/Dom_practice/script.js
@@ -2,43 +2,43 @@ const users = [
   {
     id: 1,
     name: "Elena Rodriguez",
-    image: "https://i.pravatar.cc/150?img=5",
+    image: "https://images.unsplash.com/photo-1741805190401-a95e551b443d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fDJkJTIwcGFpbnRlZCUyMGNyZWVwJTIwaWxsdXN0cmF0aW9ufGVufDB8fDB8fHww",
     bio: "Frontend Developer passionate about accessibility and pixel-perfect design. Loves coding with a view."
   },
   {
     id: 2,
     name: "Liam Chen",
-    image: "https://i.pravatar.cc/150?img=11",
+    image: "https://images.unsplash.com/photo-1740510293793-de8b916bb5df?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fDJkJTIwcGFpbnRlZCUyMGNyZWVwJTIwaWxsdXN0cmF0aW9ufGVufDB8fDB8fHww",
     bio: "Full Stack Engineer building scalable systems. Always learning the latest JavaScript frameworks."
   },
   {
     id: 3,
     name: "Sarah Jenkins",
-    image: "https://i.pravatar.cc/150?img=9",
+    image: "https://plus.unsplash.com/premium_photo-1755624281500-18d6fe75559c?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fDJkJTIwcGFpbnRlZCUyMGNyZWVwJTIwaWxsdXN0cmF0aW9ufGVufDB8fDB8fHww",
     bio: "UX Researcher who translates user needs into intuitive digital experiences. Coffee enthusiast."
   },
   {
     id: 4,
     name: "Marcus Johnson",
-    image: "https://i.pravatar.cc/150?img=13",
+    image: "https://images.unsplash.com/photo-1741805190358-aff8c1c1d72a?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fDJkJTIwcGFpbnRlZCUyMGNyZWVwJTIwaWxsdXN0cmF0aW9ufGVufDB8fDB8fHww",
     bio: "Digital Nomad and technical writer. Sharing knowledge one blog post at a time from around the world."
   },
   {
     id: 5,
     name: "Priya Patel",
-    image: "https://i.pravatar.cc/150?img=24",
+    image: "https://images.unsplash.com/photo-1748200100427-52921dec8597?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fDJkJTIwcGFpbnRlZCUyMGNyZWVwJTIwaWxsdXN0cmF0aW9ufGVufDB8fDB8fHww",
     bio: "Data Scientist turning complex numbers into actionable business insights. Python wizard."
   },
   {
     id: 6,
     name: "David Smith",
-    image: "https://i.pravatar.cc/150?img=53",
+    image: "https://images.unsplash.com/photo-1655834648155-f7a98ff3c49d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8MmQlMjBwYWludGVkJTIwaGVybyUyMGlsbHVzdHJhdGlvbnN8ZW58MHx8MHx8fDA%3D",
     bio: "Project Manager ensuring teams stay on track and agile. Master of organization and timelines."
   },
   {
     id: 7,
     name: "Amara Okeke",
-    image: "https://i.pravatar.cc/150?img=32",
+    image: "https://images.unsplash.com/photo-1750926013097-6c731d12be77?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fDJkJTIwcGFpbnRlZCUyMGhlcm8lMjBpbGx1c3RyYXRpb25zfGVufDB8fDB8fHww",
     bio: "Creative Director with a flair for bold typography and vibrant color palettes. Art is life."
   }
 ];
@@ -50,12 +50,12 @@ function showUsers(arr){
 
         //create img
         const img = document.createElement("img")
-        img.src = user.pic
+        img.src = user.image
         img.classList.add("bg-img");
 
         //create blurred-layer div
         const blurredLayer = document.createElement("div")
-        blurredLayer.style.backgroundImage = `url(${user.pic})`
+        blurredLayer.style.backgroundImage = `url(${user.image})`
         blurredLayer.classList.add("blurred-layer");
 
         //create content div
@@ -86,9 +86,11 @@ function showUsers(arr){
 }
 showUsers(users)
 
-document.querySelector("input").addEventListener("input", function(e){
+const inp = document.querySelector("input");
+
+inp.addEventListener("input", function(e){
     let newUsers = users.filter(user =>
-        user.name.startsWith(e.target.value)
+        user.name.startsWith(inp.value)
     );
     document.querySelector(".cards").innerHTML="";
     showUsers(newUsers);
