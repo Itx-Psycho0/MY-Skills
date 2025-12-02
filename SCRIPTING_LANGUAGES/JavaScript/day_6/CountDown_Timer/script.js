3d36246cf89b17fa90ada7a58648adfbbf526eff
Anurag Singh <anuragh3re@gmail.com>
2025-12-02 22:40:04 +0530

    complete full proj

--- SCRIPTING_LANGUAGES/JavaScript/day_6/CountDown_Timer/script.js
+++ SCRIPTING_LANGUAGES/JavaScript/day_6/CountDown_Timer/script.js
@@ -1,12 +1,19 @@
 const timerElement = document.getElementById('timer');
+const body = document.body;
 let countdown = 10;
+
+
 const intervalId = setInterval(() => {
     if (countdown >= 0) {
-        timerElement.textContent = `Countdown: ${countdown} seconds`;
+        timerElement.textContent = `${countdown} seconds`;
         countdown--;
     } else {
         clearInterval(intervalId);
         timerElement.textContent = 'Time is up!';
+        const intervalEnd = setInterval(() => {
+            const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0");
+            body.style.backgroundColor = randomColor;
+        }, 100);
     }
 }, 1000);
 
