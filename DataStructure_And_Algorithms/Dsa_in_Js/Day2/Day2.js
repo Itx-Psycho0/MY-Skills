4c18c063f0501e7e7c2d791bb50ae649d7e94361
Anurag Singh <anuragh3re@gmail.com>
2026-01-02 07:23:37 +0530

    day 2 in js dsa

---
+++ DataStructure_And_Algorithms/Dsa_in_Js/Day2/Day2.js
@@ -0,0 +1,15 @@
+// Day2 in DSa in Js
+
+// Topic: Math Functions in JavaScript
+
+console.log("Welcome to Day 2 of Data Structures and Algorithms in JavaScript!");
+
+Math.abs(-7); // 7
+
+Math.round(4.6); // 5
+
+Math.ceil(4.2); // 5
+
+Math.floor(4.8); // 4
+
+// and more math functions
