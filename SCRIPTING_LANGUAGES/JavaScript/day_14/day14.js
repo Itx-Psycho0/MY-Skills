7b12cba60257108455569dfe5b9d69894bad3f01
Anurag Singh <anuragh3re@gmail.com>
2026-01-04 15:38:03 +0530

    day 14 in js

---
+++ SCRIPTING_LANGUAGES/JavaScript/day_14/day14.js
@@ -0,0 +1,9 @@
+// ------- Day 14 ------------//
+// Object-oriented programming in JavaScript
+
+// Introduction to Object-Oriented Programming
+// Object-oriented programming, or OOP in short, is a programming paradigm based on the concept of objects. A paradigm simply means the style of the code, so how we write and organize code. We use objects to model aspects of the real world, such as a user or a to-do list item, or even more abstract features like an HTML component or some kind of data structure.
+
+// Objects can contain data, which we call properties, and also code, which we call methods. By using objects, we pack all the data and the corresponding behavior into one big block. This makes it very easy to act directly on the data.
+
+// In OOP, objects are self-contained pieces or blocks of code, like small applications on their own. We use these objects as building blocks of our applications and make objects interact with one another. These interactions happen through a so-called public interface, also called an API. This interface is basically a set of methods that code outside the objects can access to communicate with the object.
