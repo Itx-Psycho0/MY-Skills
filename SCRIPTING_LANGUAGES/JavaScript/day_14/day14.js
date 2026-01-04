30be2ca9715e7c34d31a313e079c5b1bf8ea5f42
Anurag Singh <anuragh3re@gmail.com>
2026-01-04 20:29:17 +0530

    oops concept

--- SCRIPTING_LANGUAGES/JavaScript/day_14/day14.js
+++ SCRIPTING_LANGUAGES/JavaScript/day_14/day14.js
@@ -7,3 +7,34 @@
 // Objects can contain data, which we call properties, and also code, which we call methods. By using objects, we pack all the data and the corresponding behavior into one big block. This makes it very easy to act directly on the data.
 
 // In OOP, objects are self-contained pieces or blocks of code, like small applications on their own. We use these objects as building blocks of our applications and make objects interact with one another. These interactions happen through a so-called public interface, also called an API. This interface is basically a set of methods that code outside the objects can access to communicate with the object.
+
+
+// Class
+// A class is a blueprint for creating objects. It defines the properties and methods that the objects created from the class will have.
+
+// javascript does not have built-in support for classes like some other programming languages, but we can create classes using functions and prototypes.
+
+// Instance
+// By using the class create a object that called instance
+
+// how de we actually design classes?
+
+// for making a good class we have to follow some principles of OOP
+// 1. Encapsulation
+// 2. Abstraction
+// 3. Inheritance
+// 4. Polymorphism
+
+// abstraction
+// abstraction is the concept of hiding the complex implementation details and showing only the necessary parts to the user. It helps in reducing complexity and increases efficiency.
+
+// Encapsulation
+// Keeping prop. amd methods private inside a class, so they are not accessible from outside the class. Some methods can be exposed as a public interface (API) to the outside world.
+
+// Inheritance
+// Inheritance is a mechanism where a new class can inherit properties and methods from an existing class. This helps in code reusability and establishing a relationship between classes.
+
+// Polymorphism
+// Polymorphism is the ability of different classes to be treated as instances of the same class through a common interface. It allows methods to do different things based on the object it is acting upon.
+
+
