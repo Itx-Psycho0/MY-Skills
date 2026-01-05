afa9209fbe4bd064f5feab090b2fc0294cd3dd78
Anurag Singh <anuragh3re@gmail.com>
2026-01-05 15:31:24 +0530

    Tailwind with react

--- SCRIPTING_LANGUAGES/React/Day3/Project 1/src/App.jsx
+++ SCRIPTING_LANGUAGES/React/Day3/Project 1/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="flex flex-col items-center gap-6 p-7 md:flex-row rounded-2xl">
+        <div>
+          <img
+            class="size-48 shadow-xl rounded-md"
+            alt=""
+            src="1093511.png"
+          />
+        </div>
+        <div class="flex items-center md:items-start">
+          <span class="text-2xl font-medium">Class Warfare</span>
+          <span class="font-medium text-sky-500">The Anti-Patterns</span>
+          <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+            <span>No. 4</span>
+            <span>·</span>
+            <span>2025</span>
+          </span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
