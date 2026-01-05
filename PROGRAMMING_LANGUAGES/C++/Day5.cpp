18a9538bbca2f3b17767bba9ca99eb8f034fc02b
Anurag Singh <anuragh3re@gmail.com>
2026-01-05 18:06:58 +0530

    loops

--- PROGRAMMING_LANGUAGES/C++/Day5.cpp
+++ PROGRAMMING_LANGUAGES/C++/Day5.cpp
@@ -2,7 +2,23 @@
 //for adding external libraries we use double quotes
 //.h means header file
 // #include "raylib.h"
+#include<cstdio>
 int main(){
     // InitWindow(800,600,"My First Window");
+    for(int i=0;i<5;i++){
+        //code to be repeated
+        printf("Hello Guys %d\n",i);
+    }
     return 0;
 }
+// Loops
+// for(int i=0;i<5;i++){
+//     //code to be repeated
+// }
+// while(condition){
+//     //code to be repeated    
+// }
+
+// do{
+//     //code to be repeated
+// }while(condition);
