da088b74edc6bdc0836029a5d7d2140a45dc0140
Anurag Singh <anuragh3re@gmail.com>
2026-01-05 17:32:22 +0530

    try ray lib

---
+++ PROGRAMMING_LANGUAGES/C++/Day5.cpp
@@ -0,0 +1,8 @@
+// why in quotes? because raylib.h is not in standard include directories
+//for adding external libraries we use double quotes
+//.h means header file
+// #include "raylib.h"
+int main(){
+    // InitWindow(800,600,"My First Window");
+    return 0;
+}
