ecab2a618128dea2bba9f8106995f9e79a15e04e
Anurag Singh <anuragh3re@gmail.com>
2026-01-07 00:55:41 +0530

    asynchronous

---
+++ SCRIPTING_LANGUAGES/JavaScript/day_15/day15.js
@@ -0,0 +1,9 @@
+// Day 5 
+// Topic :- Asynchronous Javascript
+
+// First we know what is synchronous:-
+// so up to until what we write all is synchronous code , that are executed by line by line, that uses thread of execution, but why need asynchronous then if it have one problem that is long running operation block code execution, that why we need async..
+
+//Asynchronous code is executed after a task that runs in the "background" finishes.
+// and code will non-blocking
+//Execution doesn't wait for an asynchronous task to finish its work;
