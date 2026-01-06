a09aa50c03c9db3c55193118cdd8ef0eca820308
Anurag Singh <anuragh3re@gmail.com>
2026-01-07 01:12:27 +0530

    APIs,AJAX

--- SCRIPTING_LANGUAGES/JavaScript/day_15/day15.js
+++ SCRIPTING_LANGUAGES/JavaScript/day_15/day15.js
@@ -7,3 +7,17 @@
 //Asynchronous code is executed after a task that runs in the "background" finishes.
 // and code will non-blocking
 //Execution doesn't wait for an asynchronous task to finish its work;
+
+
+//AJAX Calls
+//Asynchronous JavaScript And Xml: Allow us to Communicate with the remote web servers in an asynchronous way. With AJAX calls, we can request data from web servers dynamically,so without reloading the page so we can use the data in an Application dynamically.
+
+// HOW AJAX works?
+// Client(browser)---> request data (GET/POST/ETC)---> Web server --> response data ---> Client(browser) :- (this is happend asynchronously in the background)
+
+
+// API
+//Application Programming Interface: Piece of Software that can be used by another piece of software, in order to allow applications to talk to each other.
+// There are many APIs :- like :- DOM, GEOLOCATION, OWN CLASS API,ONLINE APIs, etc
+// Online APIs:- Application running on a server, that receives request for data, and sends data back as response.
+//We can build our own Web APIs(requires back-end development, e.g node.js) or use 3rd-party APIs.
