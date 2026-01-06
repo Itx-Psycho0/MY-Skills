e1d8f2310db418af528f950328ef337c1b2482a1
Anurag Singh <anuragh3re@gmail.com>
2026-01-06 15:30:55 +0530

    react

---
+++ SCRIPTING_LANGUAGES/React/Day4/theory.jsx
@@ -0,0 +1,23 @@
+// Day 4
+// Theory
+
+// What is ReactJs?
+// ReactJs is a popular JavaScript library for building user interfaces, particularly single-page applications where you need a fast and interactive user experience. It was developed by Facebook and is maintained by Facebook and a community of individual developers and companies.
+
+// Why ReactJs?
+// 1. Component-Based Architecture: React allows developers to build encapsulated components that manage their own state, then compose them to make complex UIs. This promotes reusability and easier maintenance.
+// 2. Virtual DOM: React uses a virtual DOM to optimize updates to the actual DOM, resulting in improved performance and a smoother user experience.
+// 3. Declarative Syntax: React's declarative approach makes it easier to understand and predict how the UI will look based on the current state of the application.
+// 4. Strong Community and Ecosystem: React has a large and active community, which means plenty of resources, libraries, and tools are available to help developers.
+// 5. JSX Syntax: React uses JSX, a syntax extension that allows developers to write HTML-like code within JavaScript, making it easier to visualize the structure of the UI.
+
+//React is build by Facebook and was initially released in 2013. Since then, it has gained widespread adoption and has become one of the most popular libraries for front-end development.
+
+// What is JSX? javascript + Html
+// JSX (JavaScript XML) is a syntax extension for JavaScript that allows you to write HTML-like code within your JavaScript code. It is commonly used with React to describe the structure of the user interface in a more readable and declarative way.
+// JSX allows you to create React elements using a syntax that closely resembles HTML, making it easier to visualize the UI structure. Under the hood, JSX is transpiled into regular JavaScript function calls that create React elements.
+
+function greet(){
+    console.log("Hello World")
+}
+<greet />
