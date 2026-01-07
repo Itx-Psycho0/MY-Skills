f7d68d51d478c7382496321bc7fca3412fa5589d
Anurag Singh <anuragh3re@gmail.com>
2026-01-07 12:38:03 +0530

    COnsuming promises

--- SCRIPTING_LANGUAGES/JavaScript/day_15/day15.js
+++ SCRIPTING_LANGUAGES/JavaScript/day_15/day15.js
@@ -31,39 +31,39 @@
 
 //PROJECT FOR LEARNING
 ///////////////////////////////////////
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries');
 
-const getCountryData = function(country){
+// const getCountryData = function(country){
 
-    const request = new XMLHttpRequest(); // first way to do ajax call or you can say older way
-    request.open('GET',`https://restcountries.com/v2/name/${country}`)
-    request.send();
-    // console.log(request.responseText)
-    request.addEventListener('load',function(){
-        // console.log(this.responseText)
-        const [data] = JSON.parse(this.responseText)
-        console.log(data)
+//     const request = new XMLHttpRequest(); // first way to do ajax call or you can say older way
+//     request.open('GET',`https://restcountries.com/v2/name/${country}`)
+//     request.send();
+//     // console.log(request.responseText)
+//     request.addEventListener('load',function(){
+//         // console.log(this.responseText)
+//         const [data] = JSON.parse(this.responseText)
+//         console.log(data)
 
-        const html = `<article class="country">
-            <img class="country__img" src="${data.flag}" />
-            <div class="country__data">
-                <h3 class="country__name">${data.name}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫</span>${(  +data.population / 1000000).toFixed(1)}</p>
-                <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-                <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-            </div>
-            </article>`;
+//         const html = `<article class="country">
+//             <img class="country__img" src="${data.flag}" />
+//             <div class="country__data">
+//                 <h3 class="country__name">${data.name}</h3>
+//                 <h4 class="country__region">${data.region}</h4>
+//                 <p class="country__row"><span>👫</span>${(  +data.population / 1000000).toFixed(1)}</p>
+//                 <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//                 <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//             </div>
+//             </article>`;
 
-        countriesContainer.insertAdjacentHTML('beforeend',html)
-        countriesContainer.style.opacity = 1;
-    })
-}
+//         countriesContainer.insertAdjacentHTML('beforeend',html)
+//         countriesContainer.style.opacity = 1;
+//     })
+// }
 
-// getCountryData("")
-getCountryData("bharat")
-getCountryData("China")
+// // getCountryData("")
+// getCountryData("bharat")
+// getCountryData("China")
 
 
 // Callback hell
@@ -143,4 +143,84 @@ const getCountryAndNeighbour = function (country) {
 
 // getCountryAndNeighbour('portugal');
 getCountryAndNeighbour('usa');*/
+
+
+// Promise 
+// const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+// const request = fetch('https://restcountries.com/v2/name/portugal')
+// console.log(request)
+
+// Promise:- An object is used as a placeholder for the future result of an asynchronous operation
+
+// we no longer to rely on events and callbacks passed into asynchronous functions to handle asynchronous results;
+//Instead of nesting callback, we can chain promises for a sequence of asynchronous opr: escapibg callback.
+
+//LifeCycle of promises
+// Pending(Before the future value is available)-->(async task) --> Settled(Asynchronous task has finished)-->(fulfilled,Rejected)
+
+// Consuming Promises
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+// const getCountryData = function(country){
+//     fetch(`https://restcountries.com/v2/name/${country}`).then(function(response){
+//         console.log(response)
+//         return response.json()
+//     }).then(function(data){
+//         console.log(data)
+//         renderCountry(data[0])
+// })
+// }
+// getCountryData("portugal")
+
+
+// Chaining Promises
+
+const getCountryData = function(country){
+    fetch(`https://restcountries.com/v2/name/${country}`).then(function(response){
+        console.log(response)
+        return response.json()
+    }).then(function(data){
+        console.log(data)
+        renderCountry(data[0])
+        const neighbour = data[0].borders?.[0]
+        if(!neighbour) return
+        return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
+        }).then(function(response){
+            return response.json()
+        }).then(function(data){
+            console.log(data)
+            renderCountry(data,"neighbour")
+
+})
+}
+getCountryData("bharat")
+
+// Handling Rejected Promises
 
