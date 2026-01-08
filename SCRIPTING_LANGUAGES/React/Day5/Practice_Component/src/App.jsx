706e1d84cf6ec946db6b079e9afd7b15bb6437be
Anurag Singh <anuragh3re@gmail.com>
2026-01-08 18:51:12 +0530

    Main Component

--- SCRIPTING_LANGUAGES/React/Day5/Practice_Component/src/App.jsx
+++ SCRIPTING_LANGUAGES/React/Day5/Practice_Component/src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./Footer"
 import Header from "./Header"
+import MainBody from "./MainBody"
 
 function App() {
   return(
     <>
     <Header/>
     <Footer/>
+    <MainBody/>
     
     </>
   )
---
+++ SCRIPTING_LANGUAGES/React/Day5/Practice_Component/src/MainBody.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainBody = () => {
+  return (
+    <main>
+        <h2>Main Content</h2>
+        <p>Lets make something impressive using <strong>React.js</strong></p>
+    </main>
+  )
+}
+
+export default MainBody
