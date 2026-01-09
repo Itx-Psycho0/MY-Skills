1208cc85a801ce9eae7d147f9824abbcb3de619a
Anurag Singh <anuragh3re@gmail.com>
2026-01-09 07:55:31 +0530

    conditional rendering

--- SCRIPTING_LANGUAGES/React/Day7/conditional_rendering/src/App.jsx
+++ SCRIPTING_LANGUAGES/React/Day7/conditional_rendering/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+const validPassword = () => <h1>Password is valid</h1>
+const invalidPassword = () => <h1>Password is invalid</h1>
+const Password = ({isvalid}) => {
+  if(isvalid){
+    return validPassword()
+  }
+  return invalidPassword()
+}
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style = {{backgroundColor:'aliceblue',width:100,height:100}}>
+      <Password isvalid = {true} />
+      
+    </div>
   )
 }
 
 export default App
+
+// Conditonal rendering allows us to dynamically display different UI components or content based on specific conditions. This enables us to create more interactive and responsive user experiences
