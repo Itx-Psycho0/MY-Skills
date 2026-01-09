73ec9e27b460ac61c995a9f63e7235e3d1d91fdb
Anurag Singh <anuragh3re@gmail.com>
2026-01-09 10:06:10 +0530

    portal

---
+++ SCRIPTING_LANGUAGES/React/Day8/Portal/src/Portal.jsx
@@ -0,0 +1,17 @@
+// Portal is a feature that allows you to render a child component into a DOM node that exists outside the hierarchy of the parent component. This can be useful for scenarios like modals, tooltips, or dropdowns, where you want to break out of the usual parent-child structure and render in a different part of the DOM.
+
+import React from 'react'
+import { useState } from 'react'
+
+const Portal = () => {
+    const [inputValue,setInputValue]= useState('')
+    const [copied, setCopied] = useState(false)
+  return (
+    <div>
+        
+      
+    </div>
+  )
+}
+
+export default Portal
