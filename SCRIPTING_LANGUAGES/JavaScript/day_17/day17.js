e26f9b8de9b28f19a117b07eb39ade8baaaf7f47
Anurag Singh <anuragh3re@gmail.com>
2026-01-11 17:41:47 +0530

    learn import export

---
+++ SCRIPTING_LANGUAGES/JavaScript/day_17/day17.js
@@ -0,0 +1,11 @@
+// Modern js development
+
+// we have modules and 3rd party package by npm, then we compress it in bundle(join all the modules into one file), then transpiling/polyfilling(means converting es6 code in to older version like es5 etc so older browser understand it. ) using BABEL so all it called build process. then all it become js bundle called production it is done by parcel or webpack(these are dev tools those help to build our applications).
+
+// Module:- Reusable piece of code that encapsulates implementation details;
+//usually a standalone file, but it doesn't have to be.
+
+import "./shoppingCart.js"
+console.log("Importing Module")
+
+
