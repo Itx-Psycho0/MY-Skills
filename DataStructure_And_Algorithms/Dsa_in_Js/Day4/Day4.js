2e92f63f570d334ccccae14640f5b01de9a9eae0
Anurag Singh <anuragh3re@gmail.com>
2026-01-11 14:46:51 +0530

    (O)n

--- DataStructure_And_Algorithms/Dsa_in_Js/Day4/Day4.js
+++ DataStructure_And_Algorithms/Dsa_in_Js/Day4/Day4.js
@@ -5,4 +5,7 @@
 //Algorithms:-
 // Is the step by step process to solving that problem.
 
-// 
+// Big O notation:
+// Big O notation helps  us understand how long an algorithm will take to run or how much memory it will need as the amount of data it handles grows.
+
+//(O)n :- Signifies that the execution time of the algorithm grows lineaerly in proportion to the size of the input data(n).
