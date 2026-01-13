24f2adcdfddde37ae53c5d14e5e3ef9e72492b16
Anurag Singh <anuragh3re@gmail.com>
2026-01-13 19:30:48 +0530

    day7

---
+++ PROGRAMMING_LANGUAGES/C++/Day7.cpp
@@ -0,0 +1,21 @@
+// Day 7
+
+//1- bit = every each binary value
+// 8- bits = 1 byte
+// 4 bytes = int,float
+// 1 bytes = char,bool
+// 8 bytes = Double
+
+// cout << "me";
+#include <iostream>
+using namespace std;
+int main(){
+    double n = 100.99;
+    int x = (int)n;
+    int y;
+    cin >> y;
+    cout << x << endl;
+    cout << "me\nyou"<< endl;
+    return 0;
+
+}
