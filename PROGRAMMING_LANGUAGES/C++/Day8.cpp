18248401958c5447a36370082fcb8e5f8d7036ab
Anurag Singh <anuragh3re@gmail.com>
2026-01-15 12:57:39 +0530

    if-else

--- PROGRAMMING_LANGUAGES/C++/Day8.cpp
+++ PROGRAMMING_LANGUAGES/C++/Day8.cpp
@@ -1,1 +1,13 @@
 // Day 8
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    if(n>=0){
+        cout << "Is +ve" <<"\n";
+    }else{
+        cout << "Is -ve" <<"\n";
+    }
+}
