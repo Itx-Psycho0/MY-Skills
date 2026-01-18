2ac23d182622614c20a984102fdc59e8abfd585b
Anurag Singh <anuragh3re@gmail.com>
2026-01-18 19:57:38 +0530

    count fn

--- SCRIPTING_LANGUAGES/React/Day9/Again_Start/api_fetching/src/App.jsx
+++ SCRIPTING_LANGUAGES/React/Day9/Again_Start/api_fetching/src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { use } from 'react'
 import { useState } from 'react'
 
 const App = () => {
   const [advice, setAdvice] = useState("")
+  const [count, setCount] = useState(0)
   async function fetchSomeAdvice() {
     const url = 'https://api.adviceslip.com/advice'
     const response = await fetch(url)
     const data = await response.json()
     setAdvice(data.slip.advice)
+    setCount(count + 1)
   }
   return (
     <div>
       <h1>{advice}</h1>
-      <button onClick={fetchSomeAdvice}>Fetch Advice</button>  
+      <button onClick={fetchSomeAdvice}>Fetch Advice</button> 
+      <p>You have read <strong>{count}</strong> pieces of advice.</p> 
     </div>
   )
 }
