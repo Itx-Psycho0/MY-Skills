2a45c7c3e92b5f2c4d620833a53ee3706b27d70b
Anurag Singh <anuragh3re@gmail.com>
2026-01-18 19:34:16 +0530

    fetch some advice

--- SCRIPTING_LANGUAGES/React/Day9/Again_Start/api_fetching/src/App.jsx
+++ SCRIPTING_LANGUAGES/React/Day9/Again_Start/api_fetching/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [advice, setAdvice] = useState("")
+        async function fetchSomeAdvice() {
+        const url = 'https://api.adviceslip.com/advice'
+        const response = await fetch(url)
+        const data = await response.json()
+        setAdvice(data.slip.advice)
+    }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={fetchSomeAdvice}>Fetch Advice</button>  
+    </div>
   )
 }
 
 export default App
+
