5d9c5afc4e4daa85f4d0beac59b30acadfac0f65
Anurag Singh <anuragh3re@gmail.com>
2026-01-18 12:00:26 +0530

    push

--- SCRIPTING_LANGUAGES/React/Day9/learn_stage/src/App.jsx
+++ SCRIPTING_LANGUAGES/React/Day9/learn_stage/src/App.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const App = () => {
+  useEffect(()=>{
+
+
+
+    return()=>{}
+  },[]);// this will only run on page load once it is like startfn
   return (
     <div>
       
