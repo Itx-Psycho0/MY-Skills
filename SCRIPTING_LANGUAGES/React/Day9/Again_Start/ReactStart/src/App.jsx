bdaf6d1338d0e2fb02f77f4e1a5c0c2cca6ae94a
Anurag Singh <anuragh3re@gmail.com>
2026-01-19 16:23:21 +0530

    theory

--- SCRIPTING_LANGUAGES/React/Day9/Again_Start/ReactStart/src/App.jsx
+++ SCRIPTING_LANGUAGES/React/Day9/Again_Start/ReactStart/src/App.jsx
@@ -16,3 +16,18 @@ export default App
 // react is really good in
 // Rendering components on a webpage(UI) based on their current state.
 // Keeping the UI in sync with state, by re-rendering(reacting) when state changes.
+
+// Escape hatches page on react documentation , in advance react read later, also a reference section.
+
+// Two option for setup react by create-react-app and vite.
+
+// create-react-app - complete "starter kit" for react application
+//Everything is already configured: Eslint, prettier, Jest, etc.
+//uses slow and outed technologies (i.e. webpack)
+//use for tut or experiments, but dont use for real world app.
+
+
+//Vite - Modern build tool that contains a template for setting up react applications.
+//Need to manually set up EsLint(and others)
+//use for modern real world apps.
+//Extremely fast hot module replacement(HMR) and bundling
