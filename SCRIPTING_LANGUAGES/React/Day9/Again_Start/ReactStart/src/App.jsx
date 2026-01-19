7068ba736f51321b12d987b003aad7cbc989c4fd
Anurag Singh <anuragh3re@gmail.com>
2026-01-19 14:24:20 +0530

    what is react

--- SCRIPTING_LANGUAGES/React/Day9/Again_Start/ReactStart/src/App.jsx
+++ SCRIPTING_LANGUAGES/React/Day9/Again_Start/ReactStart/src/App.jsx
@@ -9,3 +9,6 @@ const App = () => {
 }
 
 export default App
+// react - based on components, declarative, state-driven-js library.
+// frameworks those make on top of react is next js and remix.
+//react was created in 2011 by jordan walke, an engineer working at facebook at the time.
