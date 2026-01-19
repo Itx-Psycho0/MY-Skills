5d5c6db0d513da98cbc2c1931c5f472891872f7b
Anurag Singh <anuragh3re@gmail.com>
2026-01-19 19:04:24 +0530

    baiscs

---
+++ PROGRAMMING_LANGUAGES/C#/Day1.cs
@@ -0,0 +1,14 @@
+// Day 1
+//What is C#
+// The modern, innovative, open-source programming language for building all your apps.
+
+// build in 2000 by microsoft
+
+// Common Errors
+// Incorrect Syntax, Wrong Case, Variable not Initialized, connot convert type, Different Scope, Require an Instance of a class, variable not changing, cannot access, private, Missing Namespace.
+
+Console.WriteLine("Hello World");
+
+// variables - container
+int number = 3;
+Console.WriteLine(number);
