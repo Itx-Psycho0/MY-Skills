bba76f494b9e9c9c1439997b074ca803b6362e68
Anurag Singh <anuragh3re@gmail.com>
2026-01-19 16:04:17 +0530

    Pure react

--- SCRIPTING_LANGUAGES/React/Day9/Again_Start/ReactStart/src/App.jsx
+++ SCRIPTING_LANGUAGES/React/Day9/Again_Start/ReactStart/src/App.jsx
@@ -12,3 +12,7 @@ export default App
 // react - based on components, declarative, state-driven-js library.
 // frameworks those make on top of react is next js and remix.
 //react was created in 2011 by jordan walke, an engineer working at facebook at the time.
+
+// react is really good in
+// Rendering components on a webpage(UI) based on their current state.
+// Keeping the UI in sync with state, by re-rendering(reacting) when state changes.
