5a2bc08814ec78469ce9ece2021017851411df4f
Anurag Singh <anuragh3re@gmail.com>
2026-01-19 17:02:26 +0530

    if else ternary

--- PROGRAMMING_LANGUAGES/C++/Day8.cpp
+++ PROGRAMMING_LANGUAGES/C++/Day8.cpp
@@ -7,9 +7,24 @@ int main(){
     cin >> n;
     if(n>=0){
         cout << "Is +ve" <<"\n";
-    }else{
+    }else if(n<0){
         cout << "Is -ve" <<"\n";
+    }else{
+        cout << "invalid output" <<"\n";
     }
 
-    
+    //Find lowercase and uppercase
+    char ch;
+    cin >> ws >> ch;
+    if(ch >= 'a' && ch <= 'z'){
+        cout << "Lowercase" <<"\n";
+    }else if(ch >= 'A' && ch <= 'Z'){
+        cout << "Uppercase" <<"\n";
+    }else{
+        cout << "invalid output" <<"\n";
+    }
+    //ternary statement - condition?stm1:stm2;
+    return 0; 
 }
+
+// char is stored in integer form in memory.
