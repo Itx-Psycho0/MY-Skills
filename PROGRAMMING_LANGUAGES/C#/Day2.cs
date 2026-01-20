385d99381bb781fd9c581880e77bf0e614ad8bf2
Anurag Singh <anuragh3re@gmail.com>
2026-01-20 18:57:43 +0530

    functions

--- PROGRAMMING_LANGUAGES/C#/Day2.cs
+++ PROGRAMMING_LANGUAGES/C#/Day2.cs
@@ -55,5 +55,47 @@ switch (expression)
 
 // && , ||
 
+// functions
+// these are blocks of code that allow you to run the same logic wihtout copy pasted code
+
+// static fn in class are main fns
+
+// void fn
+void Fn1(string param)
+{
+    // this fn doesn't return anything
+    Console.WriteLine(param);
+}
+Fn1("hello");
+int Addnum(int num1, int num2)
+{
+    return num1 + num2;
+}
+// this fn return 
+Console.WriteLine(Addnum(2,3));
+
+int Addnum(int num1, int num2, int num3)
+{
+    return num1 + num2 + num3;
+}
+
+Console.WriteLine(Addnum(2,3,4));
+// if we change params then it make another variation of that fn.
+
+bool IsPos(int num)
+{
+    if (num > 0)
+    {
+        return true;
+    }
+}
+Console.WriteLine(IsPos(2));
+
+// if we pass any var as param in fn and want to update in fn then its update copy of it not the original one, but if there is reference type , then they will be update the original one.
+
+
+
+
+
 
 
