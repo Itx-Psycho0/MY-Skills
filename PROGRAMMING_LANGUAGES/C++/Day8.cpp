77ca38e47396ff09203e37cc3480271c6c0ca70e
Anurag Singh <anuragh3re@gmail.com>
2026-01-20 07:42:26 +0530

    loops in c++

--- PROGRAMMING_LANGUAGES/C++/Day8.cpp
+++ PROGRAMMING_LANGUAGES/C++/Day8.cpp
@@ -24,6 +24,69 @@ int main(){
         cout << "invalid output" <<"\n";
     }
     //ternary statement - condition?stm1:stm2;
+    cout << "conditions ended" <<"\n";
+
+    //Loops
+    // while loop
+    cout << "while loop" <<"\n";
+    int i;
+    cin >> i;
+    //cout << i <<"\n";
+    while(i<6){
+        cout<< i <<"\n";
+        i++;
+    }
+
+    // for loop
+    cout << "for loop" <<"\n";
+    int j{3};
+    for(int i = 1; i <= 10; i++){
+        cout << j*i << "\n";
+    }
+    
+
+    //Q - sum of number to n
+    int z;
+    cin >> z;
+    int sum = 0;
+    for(int i = 1; i <= z; i++){
+        sum += i;
+    }
+    cout << sum <<"\n";
+
+    //Q - sum of all odd numbers 1 to n
+    int x;
+    cin >> x;
+    int sum1 = 0;
+    for(int i = 1; i <= x; i++){
+        if(i%2 != 0){
+            sum1 += i;
+        }
+    }
+    cout << sum1 <<"\n";
+
+    // do while
+    do{
+        cout << "do while" <<"\n";
+    }while(false);
+
+    // Q - prime number or not
+    int num;
+    cin >> num;
+    bool isPrime = true;
+    for(int i = 2; i < num; i++){
+        if(num%i == 0){
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime){
+        cout << "Prime" <<"\n";
+    }else{
+        cout << "Not Prime" <<"\n";
+    }
+
+    //break , continue
     return 0; 
 }
 
