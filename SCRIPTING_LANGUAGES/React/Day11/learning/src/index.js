43f026c08cfe4e7b36307dc132dd3d15e03f3a89
Anurag Singh <anuragh3re@gmail.com>
2026-01-21 09:19:35 +0530

    component and add css and data and resources

--- SCRIPTING_LANGUAGES/React/Day11/learning/src/index.js
+++ SCRIPTING_LANGUAGES/React/Day11/learning/src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import pizzaData from "./data";
 
-function App(){
-    return <h1>Hello World!</h1>
-
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Pizza />
+    </div>
+  );
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Pizza() {
+  return <div>
+    <h2>Pizza</h2>
+    <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+    <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+  </div> ;
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
