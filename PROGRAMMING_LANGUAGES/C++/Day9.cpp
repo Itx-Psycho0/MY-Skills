9c639fa724c5e737a4a6edc163e2d313b5eda8b7
Anurag Singh <anuragh3re@gmail.com>
2026-01-21 07:59:58 +0530

    questions

--- PROGRAMMING_LANGUAGES/C++/Day9.cpp
+++ PROGRAMMING_LANGUAGES/C++/Day9.cpp
@@ -7,8 +7,46 @@ using namespace std;
 int main(){
     for(int i =1; i < 5; i++){
         for(int j = 1; j < 5; j++){
-            cout << j;
+            if(j == i){
+                cout << 0;
+            }else{
+                cout << j;
+            }
+            
         }
         cout << "\n";
     }
+    char num = 'a';
+    for(int i =1; i <5; i++){
+        for(int j = 0; j < 4; j++){
+            cout << num << " ";
+            num++;
+        }
+        cout << "\n";
+    
+    }
+
+    // Triangle pattern
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < i+1; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    char ch = 'a';
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < i+1; j++){
+            cout << ch<<" ";
+            ch++;
+        }
+        cout << "\n";
+    }
+    for(int i = 0; i < 4; i++){
+        
+        for(int j = i+1; j >= 1; j--){
+            cout << j<<" ";
+        }
+        cout << "\n";
+    }
+
 }
