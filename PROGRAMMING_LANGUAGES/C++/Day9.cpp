edb02325d95efa10c2decd6206a869e1a095429e
Anurag Singh <anuragh3re@gmail.com>
2026-01-21 07:30:13 +0530

    day 9 in c++

---
+++ PROGRAMMING_LANGUAGES/C++/Day9.cpp
@@ -0,0 +1,14 @@
+//Day 9
+// Patterns
+
+// Square Pattern
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i =1; i < 5; i++){
+        for(int j = 1; j < 5; j++){
+            cout << j;
+        }
+        cout << "\n";
+    }
+}
