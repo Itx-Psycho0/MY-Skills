768b2eaf4e05b9f646ecc84d0477e4ca6462a32a
Anurag Singh <anuragh3re@gmail.com>
2026-01-22 12:05:47 +0530

    profile card v1 done

--- SCRIPTING_LANGUAGES/React/Day11/profile-card/src/index.js
+++ SCRIPTING_LANGUAGES/React/Day11/profile-card/src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
+
 function App() {
   return (
     <div className="card">
@@ -16,6 +17,42 @@ function App() {
     </div>
   );
 }
+
+function Avatar() {
+    return(
+        <div>
+            <img  className="avatar" src="pfp.jpg" alt="profile picture" />
+        </div>
+    )
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>I am who?</h1>
+      <p>Correction: Who am I?</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill = {"javaScript"} />
+      <Skill skill = {"CSS"}/>
+      <Skill skill = {"HTML"}/>
+    </div>
+  );
+}
+
+function Skill  (props) {
+  return (
+    <div className="skill" >
+        <p >{props.skill}</p>
+    </div>
+  );
+}
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
