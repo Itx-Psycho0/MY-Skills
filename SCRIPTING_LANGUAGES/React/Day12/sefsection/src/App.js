11e62da5e746797a4544ecd2bfb3723071e2952b
Anurag Singh <anuragh3re@gmail.com>
2026-01-23 08:38:38 +0530

    ui v1 done and unmounting

--- SCRIPTING_LANGUAGES/React/Day12/sefsection/src/App.js
+++ SCRIPTING_LANGUAGES/React/Day12/sefsection/src/App.js
@@ -1,3 +1,6 @@
+import React, { useState } from "react";
+
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -7,21 +10,29 @@ const messages = [
 
 
 function App() {
-  const step = 2;
+  const [step, setStep] = useState(1);
+  function handlePrevious(){
+    if(step > 1) setStep(step - 1);
+  }
+  function handleNext(){
+    if(step < 3) setStep(step + 1);
+  }
   return (
     <>
       <div className="steps">
         <div className="numbers">
-          <div className={`${step >= 1 ? "active" : ""}}`}>1</div>
-          <div className={`${step >= 2 ? "active" : ""}}`}>2 </div>
-          <div className={`${step >= 3 ? "active" : ""}}`}>3</div>
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2 </div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
         </div>
+
         <p className="message">Step {step}: {messages[step - 1]}</p>
         <div className="buttons">
-          <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlePrevious}>
             Previous
           </button>
-          <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          <button style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            onClick={handleNext}>
             Next
           </button>
         </div>
@@ -30,3 +41,17 @@ function App() {
   );
 }
 export default App;
+
+// State :- Data that a component can hold over time, necessary for information that it needs to remember throughout the app's lifecycle.
+//Components memory
+// State Variable / piece of state : A single variable in a component(component state)
+//Updating component state triggers React to re-render the component
+// component render make view and view combine make UI.
+
+// if we give object array in usestate we can mutate ut without setfn but it is bad practice so always do with setfn
+
+// We don't do direct dom manipulations :- Because react is declarative
+
+// State preserved throughtout re-renders
+
+// component desapier from the ui entirely called unmounting
