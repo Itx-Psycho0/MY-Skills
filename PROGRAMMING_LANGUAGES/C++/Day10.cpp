5e4176a8e4293d5d37947ce5bb9f08a09868b0da
Anurag Singh <anuragh3re@gmail.com>
2026-01-23 06:36:49 +0530

    Day 10 in c++

---
+++ PROGRAMMING_LANGUAGES/C++/Day10.cpp
@@ -0,0 +1,15 @@
+// Day 10
+// Functions
+#include <iostream>
+using namespace std;
+void printHello(){
+    //code block
+    cout << "hello" <<"\n" ;
+}
+
+int main(){
+    printHello();
+    return 0;
+}
+
+//
