71bd05ac47c8b087f6535bca5082192762ae95aa
Anurag Singh <anuragh3re@gmail.com>
2026-01-23 06:49:26 +0530

    fn

--- PROGRAMMING_LANGUAGES/C++/Day10.cpp
+++ PROGRAMMING_LANGUAGES/C++/Day10.cpp
@@ -7,9 +7,52 @@ void printHello(){
     cout << "hello" <<"\n" ;
 }
 
+int printNumber(){
+    cout << "number" << "\n";
+    return 5;
+}
+
+int sumOftwonumber(int a, int b){
+    int s = a+b;
+    return s;
+    // return a+b;
+
+}
+
+int sumOfNnumber(int n){
+    int sum = 0;
+    for(int i = 1; i <= n; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+int nFactorial(int n){
+    int fact = 1;
+    for(int i = 1; i <= n; i++){
+        fact *= i;
+    }
+    return fact;
+}
+
+// Function in memory
+// in c++ we have stack memory and heap 
+// - heap is use for dynamic allocation
+// - stack use for static "
+// functions goes in stack
+// first was called by system is main
+// then main call other fun
+
+
+
 int main(){
     printHello();
+    cout << printNumber() <<"\n";
+    cout << sumOftwonumber(5,6) <<"\n";
+    cout << sumOfNnumber(5) <<"\n";
+    cout << nFactorial(5) <<"\n";
     return 0;
 }
+
 
 //
