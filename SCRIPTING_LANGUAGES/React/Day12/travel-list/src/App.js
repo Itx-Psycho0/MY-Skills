965242a54fc330bc64c9fbac944b5d2bd068da72
Anurag Singh <anuragh3re@gmail.com>
2026-01-24 11:37:20 +0530

    add objects and show and controlled elements

--- SCRIPTING_LANGUAGES/React/Day12/travel-list/src/App.js
+++ SCRIPTING_LANGUAGES/React/Day12/travel-list/src/App.js
@@ -1,11 +1,92 @@
-import React from 'react'
+import React from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
-  )
+  );
+};
+function Logo() {
+  return <h1>Far Away</h1>;
+}
+function Form() {
+  const [description, setDescription] = React.useState("");
+  const [quantity, setQuantity] = React.useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What you need for your trip</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
 }
 
-export default App
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((items) => (
+          <Item item={items} key={items.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      You have X items on your list, and you already packed X (X%) of them.
+    </footer>
+  );
+}
+
+export default App;
+
+// Controlled elements
+// by this technique react own the state and react the state not the dom
+// Define piece of state
+// we use piece of state on the element that we control 
+// then update the state variable.
