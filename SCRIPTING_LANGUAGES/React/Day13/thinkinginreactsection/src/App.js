1a6ddbba9d2f67201d39e7c3c586f42c94e9fbf3
Anurag Singh <anuragh3re@gmail.com>
2026-01-25 15:21:15 +0530

    missing data

--- SCRIPTING_LANGUAGES/React/Day13/thinkinginreactsection/src/App.js
+++ SCRIPTING_LANGUAGES/React/Day13/thinkinginreactsection/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+// ];
 
 const App = () => {
   const [items, setItems] = React.useState([]);
