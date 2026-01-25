640751a4bc0f7581957686e3bab66d8a73b2c8cc
Anurag Singh <anuragh3re@gmail.com>
2026-01-25 09:29:07 +0530

    day 12in cpp

---
+++ PROGRAMMING_LANGUAGES/C++/Day12.cpp
@@ -0,0 +1,26 @@
+// Day 12 
+// Bitwise operators
+
+// Bitwise &
+// Bitwise |
+// Bitwise ^
+// Bitwise ~
+// Bitwise <<
+// Bitwise >>
+
+// & - 0&0 - 0
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 4, b = 8;
+    cout << (a&b) <<"\n";
+    cout << (a|b) <<"\n";
+    cout << (a^b) <<"\n";
+    cout << (~a) <<"\n";
+    cout << (a<<2) <<"\n";
+    cout << (a>>2) <<"\n";
+    return 0;
+
+}
