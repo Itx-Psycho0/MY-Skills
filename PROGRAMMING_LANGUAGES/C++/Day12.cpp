dd343b5d49368c9df229a861d93f5a2fc0cdd8c1
Anurag Singh <anuragh3re@gmail.com>
2026-01-26 06:55:44 +0530

    data modifiers

--- PROGRAMMING_LANGUAGES/C++/Day12.cpp
+++ PROGRAMMING_LANGUAGES/C++/Day12.cpp
@@ -21,6 +21,21 @@ int main(){
     cout << (~a) <<"\n";
     cout << (a<<2) <<"\n";
     cout << (a>>2) <<"\n";
+    cout << sizeof(int) <<"\n";
+    cout << sizeof(long int) <<"\n";
+    cout << sizeof(short int) <<"\n";
+    cout << sizeof(long long) <<"\n";
+    unsigned int z = -10;
+    cout << z <<"\n";
+    
+
+    int x = 10, y = 20;
+
+    if(3>1){
+      cout << x <<"\n";
+    }else{
+      cout << y <<"\n";
+    }
     return 0;
 
 }
@@ -51,3 +66,13 @@ Note:
 - Bitwise AND (&), OR (|), XOR (^), NOT (~), and shift operators (<<, >>) are part of this hierarchy.
 - Assignment operators have the lowest precedence among these.
 */
+
+// Scope :-
+// Local and Global
+// local - if else, fn, block, loop
+// Global - for all access
+
+// Data Type Modifiers:-
+// change meaning of data types:
+// long >= 4bytes, short <=2bytes, long long, signed(+ and - numbers), unsigned(+ve only).
+
