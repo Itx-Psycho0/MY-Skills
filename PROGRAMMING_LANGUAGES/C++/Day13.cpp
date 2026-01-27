4e8cba7cef9225a24af9ba7b20c5c394bceb78fe
Anurag Singh <anuragh3re@gmail.com>
2026-01-27 06:35:56 +0530

    cpp day13

---
+++ PROGRAMMING_LANGUAGES/C++/Day13.cpp
@@ -0,0 +1,20 @@
+// Day 13
+// Arrays
+#include <iostream>
+using namespace std;
+int main(){
+    int marks[5];
+    marks[0] = 10;
+    marks[1] = 20;
+    marks[2] = 30;  
+    marks[3] = 40;
+    marks[4] = 50;
+    int markings[5] = {10,20,30,40,50};
+    cout << marks[0] <<"\n";
+    cout << marks[1] <<"\n";
+    cout << marks[2] <<"\n";
+    cout << marks[3] <<"\n";
+    cout << marks[4] <<"\n";
+    cout << markings <<"\n";
+    return 0;
+}
