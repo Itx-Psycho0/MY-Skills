75e3d075cc2d96f1ad15ef86e56418ebffb84d9d
Anurag Singh <anuragh3re@gmail.com>
2026-01-27 08:09:27 +0530

    accordino proj compelete

--- SCRIPTING_LANGUAGES/React/Day14/accordioncomponentv2/src/App.js
+++ SCRIPTING_LANGUAGES/React/Day14/accordioncomponentv2/src/App.js
@@ -21,11 +21,28 @@ const faqs = [
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion() {
-  return <div>TODO</div>;
+function Accordion({ data }) {
+  return <div className='accordion'>
+    {data.map((el,index) => (<AccordionItem title={el.title} text={el.text} num={index} key={el.title}/>))}
+  </div>;
+}
+
+function AccordionItem({num, title, text }) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  function handleToggle() {
+    setIsOpen(!isOpen);
+  }
+
+  return (<div className={`item ${isOpen ? 'open' : ''}` }onClick={handleToggle}>
+    <p className='number'>{num<9 ? `0${num+1}` : num+1}</p>
+    <p className='title'>{title}</p>
+    <p className='icon'>{isOpen ? '-' : '+'}</p>
+
+    {isOpen && <div className='content-box'>{text}</div>}
+  </div>);
 }
