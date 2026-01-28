61d029a0d472a5f760951844c2f594987249757c
Anurag Singh <anuragh3re@gmail.com>
2026-01-28 07:01:11 +0530

    day 13 array

--- PROGRAMMING_LANGUAGES/C++/Day13.cpp
+++ PROGRAMMING_LANGUAGES/C++/Day13.cpp
@@ -2,6 +2,32 @@
 // Arrays
 #include <iostream>
 using namespace std;
+
+
+//Linear Search
+int Linear_Search(int arr[], int n, int key){
+    for(int i = 0; i < n; i++){
+        if(arr[i] == key){
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Reverse an array
+void reverse(int arr[], int n){
+    int start = 0;
+    int end = n-1;
+    while(start < end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
+
+
+
 int main(){
     int marks[5];
     marks[0] = 10;
@@ -28,8 +54,19 @@ int main(){
         if(arr[i] > largest){
             largest = arr[i];
         }
-    
-    
     }
+    // Pass by reference
+    // In c++ a refernce is an alternative name for an object or function, and its address is the address of the pbject or fucntion it refers to. for sake of understanding we are calling it address here .
+    //chnages in original 
+
+    cout << smallest <<"\n";
+    cout << largest <<"\n";
+    cout << Linear_Search(arr,6,15) << "\n";
+    reverse(arr,6);
+    for(int i = 0; i < 6; i++){
+        cout << "reverse array" << "\n";
+        cout << arr[i] <<"\n";
+    }
+    
     return 0;
 }
