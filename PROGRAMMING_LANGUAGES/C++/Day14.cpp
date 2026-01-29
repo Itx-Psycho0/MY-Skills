84b61194c418b9d7edfc9fbf9a67aece9a5d1ae5
Anurag Singh <anuragh3re@gmail.com>
2026-01-29 19:18:05 +0530

    day 14

---
+++ PROGRAMMING_LANGUAGES/C++/Day14.cpp
@@ -0,0 +1,37 @@
+// Day 14
+// Vectors
+// vector<int> vec:
+// vector<int> vec= {1,2,3}
+//vector <int>vec(3,0)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+    vec.pop_back();
+    vec.pop_back();
+    vec.pop_back();
+    cout << vec.front() << "\n";
+    cout << vec.back() << "\n";
+    cout << vec.size() << "\n";
+    cout << vec.empty() << "\n";
+    cout << vec.capacity() << "\n";
+    cout << vec.at(1) << "\n";
+    for(int i = 0; i < vec.size(); i++){
+        cout << vec[i] << " ";
+    }
+    cout << "\n";
+    //for each loop
+    for(int x : vec){
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+//
