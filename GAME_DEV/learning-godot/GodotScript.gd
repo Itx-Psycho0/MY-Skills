3c046d13d286de9744dae20174b7fa9c78bfc108
Anurag Singh <anuragh3re@gmail.com>
2026-02-02 11:41:04 +0530

    refcounted and signals

--- GAME_DEV/learning-godot/GodotScript.gd
+++ GAME_DEV/learning-godot/GodotScript.gd
@@ -22,7 +22,26 @@ class_name Character:
         print(_name)
 
 
+    # refCounted and memory 
+    # like we make a class without extend object class and call it in other class by new() then its make memory and if it not used godot will free the memory. if we use extends Object then its will be deletd by free() function. and its not refcounted.
+
+
+    # Lifecycle callbacks
+
+    func _init()->void:
+        print("initialized")
     
+    func _enter_tree()->void:
+        print("entered tree")
+
+    func _ready()->void:
+        print("ready")
+
+    func _process(delta: float)->void:
+        pass
+
+    func _exit_tree()->void:
+        print("exited tree")
 
       
 var me: Character = Character.new("Psycho")
