957c290dae0989507ae254948a0d09aaa52148df
Anurag Singh <anuragh3re@gmail.com>
2026-02-03 06:34:20 +0530

    pair sum

--- PROGRAMMING_LANGUAGES/C++/Day16.cpp
+++ PROGRAMMING_LANGUAGES/C++/Day16.cpp
@@ -7,11 +7,27 @@ int main(){
     vector<int> arr = {2,7,11,15};
     int target;
     cin >> target;
+
+    // pair sum Brute force
     for(int i = 0; i < arr.size(); i++){
         for(int j = i+1; j<arr.size(); j++){
             if(arr[i]+arr[j]==target){
                 cout << i << " " << j << endl;
             }
+        }
+    }
+
+    // pair sum optimal
+    int i = 0, j = arr.size()-1;
+    while(i<j){
+        int psum = arr[i]+arr[j];
+        if(psum==target){
+            cout << i << " " << j << endl;
+            break;
+        }else if(psum>target){
+            j--;
+        }else{
+            i++;
         }
     }
 }
