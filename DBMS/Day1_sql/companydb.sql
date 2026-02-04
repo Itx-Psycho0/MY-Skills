422d4426e1f316991a684d36beedcebb5dfc4bd4
Anurag Singh <anuragh3re@gmail.com>
2026-02-04 11:10:07 +0530

    day 1 sql

---
+++ DBMS/Day1_sql/companydb.sql
@@ -0,0 +1,31 @@
+-- create database company_db;
+-- Use company_db;
+-- show databases;
+-- create table employee (
+-- id INT PRIMARY KEY AUTO_INCREMENT,
+--   name VARCHAR(100),
+--   role VARCHAR(100),
+--   salary DECIMAL(10,2)
+
+-- );
+-- DESC employee;
+-- INSERT INTO employee (name, role, salary)
+-- VALUES ('Alice', 'Manager', 80000),
+--        ('Bob', 'Developer', 60000);
+--        
+
+
+-- select *from employee;
+
+-- Delete from employee
+-- where emp_id = 2;
+-- select *from employee;
+
+create database school_db;
+Use school_db;
+show databases;
+
+
+
+
+
