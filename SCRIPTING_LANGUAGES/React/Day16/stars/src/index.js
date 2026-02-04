3221dfeb34b341e9026841f448b81439084d0f87
Anurag Singh <anuragh3re@gmail.com>
2026-02-04 07:48:48 +0530

    star setup

---
+++ SCRIPTING_LANGUAGES/React/Day16/stars/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Stars from './stars';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Stars />
+  </React.StrictMode>
+);
