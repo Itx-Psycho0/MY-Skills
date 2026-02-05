5a22f0823a739a12e4f0c60573ff6dfb55a2d7e7
Anurag Singh <anuragh3re@gmail.com>
2026-02-05 07:05:21 +0530

    day 17 basic 10 question

---
+++ PROGRAMMING_LANGUAGES/C++/Day17.cpp
@@ -0,0 +1,58 @@
+// Questions
+#include <iostream>
+using namespace std;
+
+void greet(){
+    cout << "Hello World!" << endl;
+}
+void greetVar(string name, int age){
+
+    cout << "Hello " << name << ", you are " << age << " years old!" << endl;
+}
+int add(int a, int b){
+    return a+b;
+}
+int areaOfrectangle(int length, int width){
+
+    cout << "Area of rectangle is: ";
+    return length*width;
+}
+int swapValues(int a, int b){
+    int temp = a;
+    a=b;
+    b=temp;
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+    return 0;
+}
+
+int convertToFahrenheit(int celsius){
+    return (celsius * 9/5) + 32;
+}
+
+int simpleInterest(int p , int r, int t){
+    return (p*r*t)/100;
+}
+int remainder(int a, int b){
+    return a%b;
+}
+
+
+int main(){
+    int age;
+    cin >> age;
+    string name;
+    cin >> name;
+    greet();
+    greetVar(name, age);
+    cout << add(5, 10) << endl;
+    cout << areaOfrectangle(5, 10) << endl;
+    swapValues(5, 10);
+    cout << convertToFahrenheit(25) << endl;
+    cout << simpleInterest(1000, 5, 2) << endl;
+    cout << remainder(10, 3) << endl;
+
+
+    return 0;
+
+}
+
