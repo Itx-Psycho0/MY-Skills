bd982f2de94ca1af1577dc6f461c40e7d010ce63
Anurag Singh <anuragh3re@gmail.com>
2026-02-16 09:52:32 +0530

    done weather app using cbc

--- SCRIPTING_LANGUAGES/React/Day19/cbc-weather/src/Weather.js
+++ SCRIPTING_LANGUAGES/React/Day19/cbc-weather/src/Weather.js
@@ -32,24 +32,21 @@ function formatDay(dateStr) {
   }).format(new Date(dateStr));
 }
 
-
-
 class App extends React.Component {
-  // constructor is used to initialize the state of the component and bind methods if necessary. It is called before the component is mounted.
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: "india",
-      displayLocation: "",
-      isLoading: false,
-    };
-    this.fetchWeather = this.fetchWeather.bind(this);
-  }
+  state = {
+    location: "",
+    isLoading: false,
+    displayLocation: "",
+    weather: {},
+  };
 
-  async fetchWeather() {
+  fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
+
     try {
       this.setState({ isLoading: true });
-    // 1) Getting location (geocoding)
+
+      // 1) Getting location (geocoding)
       const geoRes = await fetch(
         `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
       );
@@ -59,34 +56,60 @@ class App extends React.Component {
       if (!geoData.results) throw new Error("Location not found");
 
       const { latitude, longitude, timezone, name, country_code } =
-      geoData.results.at(0);
-    console.log(`${name} ${convertToFlag(country_code)}`);
-    this.setState({ displayLocation: `${name} ${convertToFlag(country_code)}` });
+        geoData.results.at(0);
 
-    // 2) Getting actual weather
+      this.setState({
+        displayLocation: `${name} ${convertToFlag(country_code)}`,
+      });
+
+      // 2) Getting actual weather
       const weatherRes = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
-    );
-    const weatherData = await weatherRes.json();
-    this.setState({ weather: weatherData.daily });
-     } catch (err) {
-    console.error(err);
-   } finally {
-    this.setState({ isLoading: false });
-   }
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      this.setState({ weather: weatherData.daily });
+    } catch (err) {
+      console.error(err);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  setLocation = (e) => this.setState({ location: e.target.value });
+
+  // useEffect []
+  componentDidMount() {
+    // this.fetchWeather();
+
+    this.setState({ location: localStorage.getItem("location") || "" });
   }
+
+  // useEffect [location]
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+
+      localStorage.setItem("location", this.state.location);
+    }
+  }
+
   render() {
     return (
       <div className="app">
-        <h1>Weather App</h1>
-        <div>
-          <input type="text" placeholder="search for loc...."
-          value={this.state.location}
-          onChange={(e)=>this.setState({location:e.target.value})}></input>
-        </div>
-        <button onClick={this.fetchWeather}>Get Weather</button>
+        <h1>Classy Weather</h1>
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
+
         {this.state.isLoading && <p className="loader">Loading...</p>}
-        {this.state.weather && <Weather weather={this.state.weather} location={this.state.displayLocation}/>}
+
+        {this.state.weather.weathercode && (
+          <Weather
+            weather={this.state.weather}
+            location={this.state.displayLocation}
+          />
+        )}
       </div>
     );
   }
@@ -94,16 +117,47 @@ class App extends React.Component {
 
 export default App;
 
-class Weather extends React.Component {
+class Input extends React.Component {
   render() {
-    const { temperature_2m_max : max, temperature_2m_min : min, weathercode: codes , time : dates} = this.props.weather;
     return (
       <div>
-        <h2>Weather in {this.props.location}</h2>
+        <input
+          type="text"
+          placeholder="Search from location..."
+          value={this.props.location}
+          onChange={this.props.onChangeLocation}
+        />
+      </div>
+    );
+  }
+}
+
+class Weather extends React.Component {
+  componentWillUnmount() {
+    console.log("Weather will unmount");
+  }
+
+  render() {
+    const {
+      temperature_2m_max: max,
+      temperature_2m_min: min,
+      time: dates,
+      weathercode: codes,
+    } = this.props.weather;
+
+    return (
+      <div>
+        <h2>Weather {this.props.location}</h2>
         <ul className="weather">
-          {dates.map((date, idx) => (
-            <Day date={date} max={max[idx]} min={min[idx]} code={codes[idx]} key={date}
-            isToday={idx === 0}/>
+          {dates.map((date, i) => (
+            <Day
+              date={date}
+              max={max.at(i)}
+              min={min.at(i)}
+              code={codes.at(i)}
+              key={date}
+              isToday={i === 0}
+            />
           ))}
         </ul>
       </div>
@@ -113,13 +167,16 @@ class Weather extends React.Component {
 
 class Day extends React.Component {
   render() {
-    const { date, code, max, min, isToday } = this.props;
+    const { date, max, min, code, isToday } = this.props;
+
     return (
       <li className="day">
-        <p className="date">{isToday ? "Today" : formatDay(date)}</p>
         <span>{getWeatherIcon(code)}</span>
-        <p>{Math.floor(min)}° &mdash; <strong>{Math.ceil(max)}°</strong></p> 
+        <p>{isToday ? "Today" : formatDay(date)}</p>
+        <p>
+          {Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong>
+        </p>
       </li>
-    )
+    );
   }
 }
