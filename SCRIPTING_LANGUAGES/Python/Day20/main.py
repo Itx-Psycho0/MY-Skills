d42d8e56bd42beeaeaeb7d6beb92cfa90e8a4f0f
Anurag Singh <anuragh3re@gmail.com>
2026-02-16 13:17:13 +0530

    tokenization

---
+++ SCRIPTING_LANGUAGES/Python/Day20/main.py
@@ -0,0 +1,10 @@
+import tiktoken
+enc = tiktoken.encoding_for_model("gpt-4o")
+text = "Hello, how are you doing today?"
+tokens = enc.encode(text)
+
+
+print(tokens)
+
+decoded =enc.decode(tokens)
+print(decoded)
