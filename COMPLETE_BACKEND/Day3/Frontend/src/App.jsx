457992035381812bddbaffe81aa3bb0c89f29949
Anurag Singh <anuragh3re@gmail.com>
2026-02-22 13:11:29 +0530

    vercel rounting define

--- COMPLETE_BACKEND/Day3/Frontend/src/App.jsx
+++ COMPLETE_BACKEND/Day3/Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import CreatePost from './pages/CreatePost'
 import Feed from './pages/Feed'
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path='/' element={<Navigate to='/feed' replace />} />
         <Route path='/create-post' element={<CreatePost />} />
         <Route path='/feed' element={<Feed />} />
         <Route path='/contact' element={<h1>Contact Page</h1>} />
