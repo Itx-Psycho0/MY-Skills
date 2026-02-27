64265d0a08f253a8b09fbac3a34f97561b0d7b8a
Anurag Singh <anuragh3re@gmail.com>
2026-02-27 14:55:23 +0530

    DSA: Recurrsion revision

--- DataStructure_And_Algorithms/Dsa_Class/Day4.cpp
+++ DataStructure_And_Algorithms/Dsa_Class/Day4.cpp
@@ -4,5 +4,37 @@ using namespace std;
 
 
 
+
+void printNto1(int n) {
+    if (n <= 0)
+        return;
+
+    cout << n << " ";
+    printNto1(n - 1);
+}
+
+void print1toN(int n){
+    if (n<=0){
+        return;
+    }
+    print1toN(n-1);
+    cout << n << " ";
+
+}
+
+
+int sumOfNaturalNUmbers(int n){
+    if (n <= 0) {
+        return 0;
+    }
+    return n + sumOfNaturalNUmbers(n-1);
+}
+
 int main(){
+    printNto1(5);
+    cout << endl;
+    print1toN(5);
+    cout << endl;
+    cout << sumOfNaturalNUmbers(5) << endl;
+    return 0;
 }
