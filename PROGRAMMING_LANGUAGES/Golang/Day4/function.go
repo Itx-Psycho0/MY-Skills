3c1a3433bc883b3743d1cfc2b45383d6d314cd38
Anurag Singh <anuragh3re@gmail.com>
2026-02-27 10:23:06 +0530

    functions

--- PROGRAMMING_LANGUAGES/Golang/Day4/function.go
+++ PROGRAMMING_LANGUAGES/Golang/Day4/function.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func concat(s1 string, s2 string) string {
 	return s1 + s2
@@ -13,6 +16,28 @@ func main() {
 	test("Zuck,", " hope that Metaverse thing works out")
 	test("Go", " is fantastic")
 	getMonthlyPrice("basic")
+	printReports(
+		"Welcome to the Hotel California",
+		"Such a lovely place",
+		"Plenty of room at the Hotel California",
+	)
+	test1(true, true)
+	test1(false, true)
+	test1(true, false)
+	test1(false, false)
+		dbErrors := []error{
+		errors.New("out of memory"),
+		errors.New("cpu is pegged"),
+		errors.New("networking issue"),
+		errors.New("invalid syntax"),
+	}
+	test("Error on database server", dbErrors, colonDelimit)
+
+	mailErrors := []error{
+		errors.New("email too large"),
+		errors.New("non alphanumeric symbols found"),
+	}
+	test("Error on mail server", mailErrors, commaDelimit)
 }
 
 func test(s1 string, s2 string) {
@@ -78,4 +103,128 @@ func yearsUntilEvents(age int) (yearsUntilAdult, yearsUntilDrinking, yearsUntilC
 		yearsUntilCarRental = 0
 	}
 	return
+}
+
+// Functions As Values
+func reformat(message string, formatter func(string) string) string {
+	message = formatter(message)
+	message = formatter(message)
+	message = formatter(message)
+	message = fmt.Sprintf("TEXTIO: %s",message)
+	return message
+
+}
+
+// Anonymous function
+
+func printReports(intro, body, outro string) {
+	printCostReport(func(m string) int {
+		return 2 * len(m)
+	}, intro)
+
+	printCostReport(func(m string) int {
+		return 3 * len(m)
+	}, body)
+
+	printCostReport(func(m string) int {
+		return 4 * len(m)
+	}, outro)
+}
+
+
+
+func printCostReport(costCalculator func(string) int, message string) {
+	cost := costCalculator(message)
+	fmt.Printf(`Message: "%s" Cost: %v cents`, message, cost)
+	fmt.Println()
+}
+
+
+// Defer statement
+func bootup() {
+	defer fmt.Println("TEXTIO BOOTUP DONE")
+	ok := connectToDB()
+	if !ok {
+		return
+	}
+	ok = connectToPaymentProvider()
+	if !ok {
+		return
+	}
+	fmt.Println("All systems ready!")
+}
+
+
+
+var shouldConnectToDB = true
+
+func connectToDB() bool {
+	fmt.Println("Connecting to database...")
+	if shouldConnectToDB {
+		fmt.Println("Connected!")
+		return true
+	}
+	fmt.Println("Connection failed")
+	return false
+}
+
+var shouldConnectToPaymentProvider = true
+
+func connectToPaymentProvider() bool {
+	fmt.Println("Connecting to payment provider...")
+	if shouldConnectToPaymentProvider {
+		fmt.Println("Connected!")
+		return true
+	}
+	fmt.Println("Connection failed")
+	return false
+}
+
+func test1(dbSuccess, paymentSuccess bool) {
+	shouldConnectToDB = dbSuccess
+	shouldConnectToPaymentProvider = paymentSuccess
+	bootup()
+	fmt.Println("====================================")
+}
+
+
+// go is blocked scoped language, so variables declared in a function are not accessible outside of that function. This is why we cannot access the variables declared in the main function from other functions.
+
+// Closures
+func adder() func(int) int {
+	var sum int
+	return func(m int)int{
+		sum += m
+		return sum
+	}
+	
+}
+
+// currying
+// getLogger takes a function that formats two strings into
+// a single string and returns a function that formats two strings but prints
+// the result instead of returning it
+func getLogger(formatter func(string, string) string) func(string, string) {
+	return func(first, second string) {
+		result := formatter(first, second)
+		fmt.Println(result)
+	}
+}
+
+// don't touch below this line
+
+func test2(first string, errors []error, formatter func(string, string) string) {
+	defer fmt.Println("====================================")
+	logger := getLogger(formatter)
+	fmt.Println("Logs:")
+	for _, err := range errors {
+		logger(first, err.Error())
+	}
+}
+
+func colonDelimit(first, second string) string {
+	return first + ": " + second
+}
+func commaDelimit(first, second string) string {
+	return first + ", " + second
 }
