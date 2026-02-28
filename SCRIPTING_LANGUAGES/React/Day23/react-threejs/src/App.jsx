03e7e6253be78b223ed842c0e0cf7350de08f87b
Anurag Singh <anuragh3re@gmail.com>
2026-02-28 15:53:07 +0530

    feature: made first scene

--- SCRIPTING_LANGUAGES/React/Day23/react-threejs/src/App.jsx
+++ SCRIPTING_LANGUAGES/React/Day23/react-threejs/src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+
+const RotatingCube = () => {
+  const meshRef = React.useRef()
+  useFrame(() => {
+    if (meshRef.current) {
+    meshRef.current.rotation.x += 0.01
+    meshRef.current.rotation.y += 0.01
+    }
+  })
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="red" emissive={"#468585"} />
+    </mesh>
+
+  )
+}
 
 const App = () => {
   return (
-    <Canvas>
-      Hello three.js
+    <Canvas style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} />
+      <directionalLight position={[0, 0, 5]} intensity={1} color={0x9cdba6} />
+      <color attach="background" args={['#f0f0f0']} />
+      <RotatingCube/>
+
     </Canvas>
   )
 }
