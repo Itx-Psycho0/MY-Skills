fcf04ab4aa5a3fe9713e09c89df205065866e3a9
Anurag Singh <anuragh3re@gmail.com>
2026-02-28 20:24:46 +0530

    feature : leva dashboard

--- SCRIPTING_LANGUAGES/React/Day23/react-threejs/src/sections/Hero.jsx
+++ SCRIPTING_LANGUAGES/React/Day23/react-threejs/src/sections/Hero.jsx
@@ -4,9 +4,23 @@ import { PerspectiveCamera } from '@react-three/drei'
 import {HackerRoom} from '../components/HackerRoom'
 import CanvasLoader from '../components/CanvasLoader'
 import { Suspense } from 'react'
+import { Leva, useControls} from 'leva'
+import { useMediaQuery } from 'react-responsive'
 
 
 const Hero = () => {
+    const controls = useControls(
+        'HackerRoom', {
+            positionX: { value: 2.5, min: -10, max: 10, step: 0.01 },
+            positionY: { value: 2.5, min: -10, max: 10, step: 0.01 },
+            positionZ: { value: 2.5, min: -10, max: 10, step: 0.01 },
+            rotationX: { value: 2.5, min: -10, max: 10, step: 0.01 },
+            rotationY: { value: 2.5, min: -10, max: 10, step: 0.01 },
+            rotationZ: { value: 2.5, min: -10, max: 10, step: 0.01 },
+            scale: { value: 2.5, min: 0.1, max: 10, step: 0.01 },
+        }
+    )
+    const isMobile = useMediaQuery({maxWidth: 768})
   return (
     <section className='min-h-screen w-full flex flex-col relative '>
         <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
@@ -14,11 +28,18 @@ const Hero = () => {
             <p className='hero_tag text-gray_gradient'>I build immersive 3D experiences</p>
         </div>
       <div className='w-full h-full absolute inset-0'>
+        <Leva/>
         <Canvas className='w-full h-full'>
             <Suspense fallback={<CanvasLoader/>}>
                 <ambientLight intensity={0.5} />
                 <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-                <HackerRoom scale={0.09} position={[0, -6, 0]} rotation={[0, Math.PI , 0]}/>
+                <HackerRoom 
+                // scale={0.09} 
+                // position={[0, -6, 0]} 
+                // rotation={[0, Math.PI , 0]}
+                position={[2, -8, 2]} 
+                rotation={[0.4, -3, 0]}
+                scale={isMobile? 0.07: 0.1}/>
                 <ambientLight intensity={1}/>
                 <directionalLight position={[10,10,10]} intensity={1} />
             </Suspense>
