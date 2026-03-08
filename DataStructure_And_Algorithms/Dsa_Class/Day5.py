847f388f8ad985f4d996f9548ba252b1ffb6db77
Anurag Singh <anuragh3re@gmail.com>
2026-03-08 19:47:41 +0530

    Learn: SLiding window ques:

--- DataStructure_And_Algorithms/Dsa_Class/Day5.py
+++ DataStructure_And_Algorithms/Dsa_Class/Day5.py
@@ -31,3 +31,13 @@ def remove_duplicates(arr):
     return arr
 
 
+# Maximum sum of subarray of size k
+def max_sum_subarray(arr, k):
+    max_sum = float('-inf')
+    window_sum = sum(arr[:k])
+    for i in range(k, len(arr)):
+        window_sum += arr[i] - arr[i-k]
+        max_sum = max(max_sum, window_sum)
+    return max_sum
+
+
