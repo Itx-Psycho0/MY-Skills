838cc2c8b71fc1b6a3706dd3e56730c88409a8eb
Anurag Singh <anuragh3re@gmail.com>
2026-03-22 05:03:06 +0530

    feat : set post method for handle post and learn post

--- COMPLETE_BACKEND/Day6/handlers/routeHandlers.js
+++ COMPLETE_BACKEND/Day6/handlers/routeHandlers.js
@@ -8,15 +8,7 @@ export async function handleGet(req,res){
    sendResponse(res, 'application/json', 200, data);
 }
 
-/*
-Challenge:
-1. Export a function called handleGet(). 
-2. It should:
-   - use getData() to get the data
-   - stringify that data
-   - use sendResponse() to serve it
-   
-Open the browser and load the sightings page to see if it works.
-*/
 
-// handlePost
+export function handlePost(req,res){
+   console.log('POST request received');
+}
--- COMPLETE_BACKEND/Day6/server.js
+++ COMPLETE_BACKEND/Day6/server.js
@@ -1,22 +1,24 @@
-import http from 'node:http';
-import fs from 'node:fs/promises';
-import { serveStatic } from './utils/serveStatic.js';
-import { handleGet } from './handlers/routeHandlers.js';
+import http from "node:http";
+import fs from "node:fs/promises";
+import { serveStatic } from "./utils/serveStatic.js";
+import { handleGet } from "./handlers/routeHandlers.js";
 
 const PORT = 8000;
 
 const __dirname = import.meta.dirname;
 
-const server = http.createServer(async(req,res) => {
-    if(req.url === '/api'){
-        if(req.method === 'GET'){
-            return await handleGet(req,res);
-        }
-    }else if(!req.url.startsWith('/api') ){
-        await serveStatic(req,res, __dirname);
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/api") {
+    if (req.method === "GET") {
+      return await handleGet(req, res);
+    } else if (req.method === "POST") {
+      handlePost(req, res);
     }
-})
+  } else if (!req.url.startsWith("/api")) {
+    await serveStatic(req, res, __dirname);
+  }
+});
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
