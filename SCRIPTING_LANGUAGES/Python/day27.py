9fb74a2bccedb186f287acf1d2e4e325b699b65e
Anurag Singh <anuragh3re@gmail.com>
2026-03-27 14:44:38 +0530

    learn : overiding oop concept

--- SCRIPTING_LANGUAGES/Python/day27.py
+++ SCRIPTING_LANGUAGES/Python/day27.py
@@ -49,5 +49,75 @@ class Archer:
         self.health -= dmg
         return self.health
 
+# Operator Overloading
+# Another kind of built-in polymorphism in Python is the ability to override how an operator works. For example, the + operator works for built-in types like integers and strings.
 
+print(3 + 4)
+# 7
+
+print("three " + "four")
+# three four
+
+# Custom classes on the other hand don't have any built-in support for those operators:
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+p1 = Point(4, 5)
+p2 = Point(2, 3)
+p3 = p1 + p2
+# TypeError: unsupported operand type(s) for +: 'Point' and 'Point'
+
+# But we can add our own support! If we create an __add__(self, other) method on our class, the Python interpreter will use it when instances of the class are being added with the + operator. The name of the second parameter (other in this example) is just a convention - you can use any valid parameter name. Here's an example:
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Point(x, y)
+
+p1 = Point(4, 5)
+p2 = Point(2, 3)
+p3 = p1 + p2
+# p3 is (6, 8)
+
+# Now, when p1 + p2 is executed, under the hood the Python interpreter just calls p1.__add__(p2).
+
+# Overriding Built-In Methods
+# Last but not least, let's take a look at some of the built-in methods we can override in Python. While there isn't a default behavior for the arithmetic operators like we just saw, there is a default behavior for printing a class instance:
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+p1 = Point(4, 5)
+print(p1)
+# prints "<Point object at 0xa0acf8>"
+
+# That's not super useful! We probably want to see the fields!
+
+# Let's teach our Point class to print itself. The __str__ method (short for "string") lets us do just that. It takes no inputs but returns a string that will be printed to the console when someone passes an instance of the class to Python's print() function.
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x},{self.y})"
+
+p1 = Point(4, 5)
+print(p1)
+# prints "(4,5)"
+
+# The __repr__ method works similarly: the difference is that it's intended for use in debugging by developers, rather than in printing strings to end users.
 
