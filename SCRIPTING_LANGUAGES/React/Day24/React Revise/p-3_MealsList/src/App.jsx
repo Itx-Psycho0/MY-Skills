ea4d928b2373404c040e1dc1c9c053af1cf77f2c
Itx-Psycho0 <anuragh3re@gmail.com>
2026-04-27 01:36:28 +0530

    feat: complete project of react mealsList

---
+++ SCRIPTING_LANGUAGES/React/Day24/React Revise/p-3_MealsList/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MealsList from './components/MealsList'
+
+const App = () => {
+  return (
+    <div>
+      <MealsList />
+    </div>
+  )
+}
+
+export default App
