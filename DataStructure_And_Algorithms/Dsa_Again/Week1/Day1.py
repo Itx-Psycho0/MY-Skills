ad98056640d59cf74b6d60a5840aae8c46b7271f
Itx-Psycho0 <anuragh3re@gmail.com>
2026-06-04 17:04:28 +0530

    again starting my journey to conquering the world

---
+++ DataStructure_And_Algorithms/Dsa_Again/Week1/Day1.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+
+
+# Sum of n natural numbers
+def sum_of_natural_numbers(n):
+    return n * (n + 1) // 2
+
+# example usage
+n = 10
+print(f"The sum of the first {n} natural numbers is: {sum_of_natural_numbers(n)}")
