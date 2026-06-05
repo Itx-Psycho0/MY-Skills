a1a872bf8f9441877af2838a4d6753def2df2b6f
Itx-Psycho0 <anuragh3re@gmail.com>
2026-06-05 21:48:55 +0530

    again starting my journey to conquering the world

--- DataStructure_And_Algorithms/Dsa_Again/Week1/Day1.py
+++ DataStructure_And_Algorithms/Dsa_Again/Week1/Day1.py
@@ -8,3 +8,19 @@ def sum_of_natural_numbers(n):
 # example usage
 n = 10
 print(f"The sum of the first {n} natural numbers is: {sum_of_natural_numbers(n)}")
+
+
+#Linear Search
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i  # Return the index of the target
+    return -1  # Return -1 if the target is not found
+
+# example usage
+arr = [5, 3, 2, 8, 1]
+target = 2
+result = linear_search(arr, target)
+if result != -1:
+    print(f"Element {target} found at index: {result}")
+else:    print(f"Element {target} not found in the array.") 
