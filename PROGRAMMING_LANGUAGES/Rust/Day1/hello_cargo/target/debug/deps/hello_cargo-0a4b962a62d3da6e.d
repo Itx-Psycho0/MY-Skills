8336c79a94a958f5be2e34d3e9a63b2dfcd7b9e2
Itx-Psycho0 <anuragh3re@gmail.com>
2026-06-12 04:58:08 +0530

    rust cargo commands

---
+++ PROGRAMMING_LANGUAGES/Rust/Day1/hello_cargo/target/debug/deps/hello_cargo-0a4b962a62d3da6e.d
@@ -0,0 +1,5 @@
+/home/psycho/Work/Learning/MY-Skills/PROGRAMMING_LANGUAGES/Rust/Day1/hello_cargo/target/debug/deps/hello_cargo-0a4b962a62d3da6e.d: src/main.rs
+
+/home/psycho/Work/Learning/MY-Skills/PROGRAMMING_LANGUAGES/Rust/Day1/hello_cargo/target/debug/deps/libhello_cargo-0a4b962a62d3da6e.rmeta: src/main.rs
+
+src/main.rs:
