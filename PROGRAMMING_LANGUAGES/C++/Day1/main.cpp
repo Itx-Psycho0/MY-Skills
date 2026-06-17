b235b7d5592affd35deffbaaa2e99ddf2b1679be
Itx-Psycho0 <anuragh3re@gmail.com>
2026-06-17 23:32:03 +0530

    Start again cpp

---
+++ PROGRAMMING_LANGUAGES/C++/Day1/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Preprocessor directive to include the iostream library
+using namespace std; // Use the standard namespace
+int main()
+{                                    // Main function - entry point of the program
+    cout << "Hello, World!" << endl; // Output statement
+    return 0;                        // Return statement indicating successful execution
+}
