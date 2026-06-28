6ed150443c525de049e1250eb2634df341583dd7
Itx-Psycho0 <anuragh3re@gmail.com>
2026-06-28 22:13:31 +0530

    Learn: start again python error and exception raising

--- SCRIPTING_LANGUAGES/Python/Day28.py
+++ SCRIPTING_LANGUAGES/Python/Day28.py
@@ -66,6 +66,82 @@ def get_player_record(player_id):
 
 main()
 
+#Raising Your Own Exceptions
+def get_player_record(player_id):
+    if player_id == 1:
+        return {"name": "Slayer", "level": 128}
+    if player_id == 2:
+        return {"name": "Dorgoth", "level": 300}
+    if player_id == 3:
+        return {"name": "Saruman", "level": 4000}
+    raise Exception("player id not found")
+
+
+
+#Don't Catch Your Own Exceptions
+# don't do this
+def craft_sword(metal_bar):
+    try:
+        if metal_bar == "bronze":
+            return "bronze sword"
+        if metal_bar == "iron":
+            return "iron sword"
+        if metal_bar == "steel":
+            return "steel sword"
+        raise Exception("invalid metal bar")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+
+
+#Different Types of Exceptions
+
+#what is important to understand is that there are different types of exceptions, and we can handle them differently depending on the situation. Some exceptions are more specific, like ZeroDivisionError (which happens when you divide by zero) or IndexError (which happens when you try to access a list element at an invalid index – either too high or too low). Others are more general, like the base Exception.
+
+# Syntax
+try:
+    10/0
+except ZeroDivisionError:
+    print("0 division")
+except Exception as e:
+    print(e)
+
+try:
+    nums = [0, 1]
+    print(nums[2])
+except IndexError:
+    print("index error")
+except Exception as e:
+    print(e)
+
+# Which will print:
+
+# 0 division
+# index error
+
+# Why Specific Exceptions Come First
+# When handling exceptions, it's important to catch the most specific ones first, because Python stops checking once it finds a matching exception handler. If you catch a more general Exception first, any specific errors will never get handled individually.
+
+# For example:
+
+try:
+    nums = [0, 1]
+    print(nums[2])
+except Exception:
+    print("An error occurred")
+except IndexError:
+    print("Index error")
+
+# In this case, the general Exception will catch the error before the IndexError can be reached, and the message "Index error" will never be printed. Always handle the most specific exception first!
+
+# Alias Exception Messages
+# As seen in the example, you can also access the error using as, like this:
+
+except Exception as e:
+    print(e)
+
+# The default behavior of print is that it will print the string representation of whatever object is passed to it. In this case, it will print the error message.
 
 
 
