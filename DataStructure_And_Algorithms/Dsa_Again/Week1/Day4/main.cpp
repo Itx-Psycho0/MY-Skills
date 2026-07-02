1f577547ebd89527b3639e2ea03decdeec64ea5a
Itx-Psycho0 <anuragh3re@gmail.com>
2026-07-02 23:01:41 +0530

    day3 3 question done

--- DataStructure_And_Algorithms/Dsa_Again/Week1/Day4/main.cpp
+++ DataStructure_And_Algorithms/Dsa_Again/Week1/Day4/main.cpp
@@ -34,6 +34,40 @@ vector<int> twoSum(vector<int> &nums, int target)
     return {};
 }
 
+bool isAnagram(string s, string t)
+{
+    if (s.length() != t.length())
+    {
+        return false;
+    }
+    unordered_map<char, int> charCount;
+    for (char c : s)
+    {
+        charCount[c]++;
+    }
+    for (char c : t)
+    {
+        if (charCount.find(c) == charCount.end() || charCount[c] == 0)
+        {
+            return false;
+        }
+        charCount[c]--;
+    }
+    return true;
+}
+
 int main()
 {
+
+    cout << "Enter the number of elements in the array: ";
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+    }
+    cout << "Contains duplicate: " << (containsDuplicate(nums) ? "true" : "false") << endl;
+    return 0;
 }
