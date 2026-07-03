71cb9bb384620808d405f83ac349d4b1e80b42e4
Itx-Psycho0 <anuragh3re@gmail.com>
2026-07-03 11:21:43 +0530

    DSA: HASHING PROBLEMS Solved day3 done

--- DataStructure_And_Algorithms/Dsa_Again/Week1/Day4/main.cpp
+++ DataStructure_And_Algorithms/Dsa_Again/Week1/Day4/main.cpp
@@ -56,6 +56,54 @@ bool isAnagram(string s, string t)
     return true;
 }
 
+int majorityElement(vector<int> &nums)
+{
+    unordered_map<int, int> countMap;
+    int majorityCount = nums.size() / 2;
+    for (int num : nums)
+    {
+        countMap[num]++;
+        if (countMap[num] > majorityCount)
+        {
+            return num;
+        }
+    }
+    return -1; // This line should never be reached if a majority element exists
+}
+
+int majorityElementBoyerMoore(vector<int> &nums)
+{
+    int count = 0;
+    int candidate = 0;
+
+    for (int num : nums)
+    {
+        if (count == 0)
+        {
+            candidate = num;
+        }
+        count += (num == candidate) ? 1 : -1;
+    }
+    return candidate;
+}
+
+vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+{
+
+    unordered_set<int> set1(nums1.begin(), nums1.end());
+    unordered_set<int> resultSet;
+
+    for (int num : nums2)
+    {
+        if (set1.count(num))
+        {
+            resultSet.insert(num);
+        }
+    }
+
+    return vector<int>(resultSet.begin(), resultSet.end());
+}
+
 int main()
 {
 
