7d74471fe73b6411d93cb086161d3a7a6e0db66f
Itx-Psycho0 <anuragh3re@gmail.com>
2026-07-04 19:01:31 +0530

    Dsa: TOday i learn about binary search pattern

--- DataStructure_And_Algorithms/Dsa_Again/Week1/Day4/main.cpp
+++ DataStructure_And_Algorithms/Dsa_Again/Week1/Day4/main.cpp
@@ -104,6 +104,52 @@ vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
     return vector<int>(resultSet.begin(), resultSet.end());
 }
 
+int BinarySearch(vector<int> &nums, int target)
+{
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return -1; // Target not found
+}
+
+int SearchInsert(vector<int> &nums, int target)
+{
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return left; // Position where the target should be inserted
+}
+
 int main()
 {
 
