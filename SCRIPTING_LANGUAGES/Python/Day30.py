36590b9adb019e8b0d4236f8f4b0f0b8fa08cb86
¨Itx-Psycho0¨ <anuragh3re@gmail.com>
2026-07-05 21:50:37 +0530

    learn:lmbda function in python

--- SCRIPTING_LANGUAGES/Python/Day30.py
+++ SCRIPTING_LANGUAGES/Python/Day30.py
@@ -7,4 +7,16 @@ def file_to_prompt(
 ) -> str:
     hope:str=to_string(file)
     return f"```\n{hope}\n```"
+   
     
+#Anonymous function or lambda function
+def file_type_getter(
+    file_extension_tuples: list[tuple[str, list[str]]],
+) -> Callable[[str], str]:
+    file_extensions_dict :dict[str,str]= {}
+
+    for file_type, extensions in file_extension_tuples:
+        for extension in extensions:
+            file_extensions_dict[extension] = file_type
+    return lambda name: file_extensions_dict.get(name,"Unknown")
+
