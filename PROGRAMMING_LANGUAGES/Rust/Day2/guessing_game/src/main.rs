0a7ee82058d93d3bbebebdc0963540570776bff5
Itx-Psycho0 <anuragh3re@gmail.com>
2026-07-07 22:01:21 +0530

    again random

--- PROGRAMMING_LANGUAGES/Rust/Day2/guessing_game/src/main.rs
+++ PROGRAMMING_LANGUAGES/Rust/Day2/guessing_game/src/main.rs
@@ -1,11 +1,14 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
     println!("You guessed: {}", guess);
+    println!("The secret number was: {}", secret_number);
 }
