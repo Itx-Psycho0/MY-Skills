8f3eb2de2b2e6b798f9c188df5f6bafbb65de57b
Itx-Psycho0 <anuragh3re@gmail.com>
2026-07-18 18:49:46 +0530

    learn: Functions Done

--- PROGRAMMING_LANGUAGES/Rust/Day5/Functions/src/main.rs
+++ PROGRAMMING_LANGUAGES/Rust/Day5/Functions/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+    let result = add(5, 10);
+    println!("The sum is: {}", result);
+
+    // Satement vs Expression
+    let x = 5; // statement
+    let y = {
+        // expression
+        let z = 10;
+        z + 1
+    };
+    println!("x: {}, y: {}", x, y);
+}
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
 }
