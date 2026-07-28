e4a2c7e9a7e01379882945e206476a4b1457454c
Itx-Psycho0 <anuragh3re@gmail.com>
2026-07-28 21:23:05 +0530

    Enums and match pattern

--- PROGRAMMING_LANGUAGES/Rust/Day12/Enums_and_PatternMatching/src/main.rs
+++ PROGRAMMING_LANGUAGES/Rust/Day12/Enums_and_PatternMatching/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::ErrorKind::Other;
+
 #[derive(Debug)]
 enum IpAddrKind {
     V4,
@@ -26,6 +28,13 @@ impl IpAddr {
     }
 }
 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
 fn main() {
     let ip1 = IpAddr::new("1.2.3.4");
     route(ip1);
@@ -38,6 +47,25 @@ fn main() {
     let some_number: Option<i32> = Some(5);
     let no_number: Option<i32> = None;
     println!("Some number: {:?}, No number: {:?}", some_number, no_number);
+
+    let penny = Coin::Penny;
+    let nickel = Coin::Nickel;
+    let dime = Coin::Dime;
+    let quarter = Coin::Quarter;
+
+    println!("Penny is worth {} cents", value_in_cents(penny));
+    println!("Nickel is worth {} cents", value_in_cents(nickel));
+    println!("Dime is worth {} cents", value_in_cents(dime));
+    println!("Quarter is worth {} cents", value_in_cents(quarter));
+
+    println!("Add 5 and Some(10): {}", add(5, Some(10)));
+
+    let some_number: Option<i32> = Some(42);
+    if let Some(value) = some_number {
+        println!("The value is: {}", value);
+    } else {
+        println!("No value found.");
+    }
 }
 
 fn route(ip: IpAddr) {
@@ -47,5 +75,20 @@ fn route(ip: IpAddr) {
 fn process_message(msg: Message) {
     match msg {
         Message::Write(text) => println!("Write message: {}", text),
+    }
+}
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Quarter => 25,
+        Coin::Dime => 10,
+    }
+}
+
+fn add(a: i32, b: Option<i32>) -> i32 {
+    match b {
+        Some(value) => a + value,
+        None => a,
     }
 }
